1b5b7a15403d5427c62066a470e6952dffd74cf7
Park <phdofnature@gmail.com>
2025-04-15 15:02:44 -0400

    Update __init__.py

--- tf2ta/__init__.py
+++ tf2ta/__init__.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 from tf2ta.utils import *
 from tf2ta.batch_size import *
-from tf2ta import learning_rate
-from tf2ta import augment
+import tf2ta.learning_rate
+import tf2ta.augment
